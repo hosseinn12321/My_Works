09202a2f59bbbf5e26591eb3fa2a9362d9c85a3e
hossein <hsotoudeh75@yahoo.com>
2024-09-19 22:07:03 +0330

    Edit Desktop View on footer

--- Front-end/Online_Shop/myscripts.js
+++ Front-end/Online_Shop/myscripts.js
@@ -6,11 +6,9 @@ function indexmenu() {
     if (x.className === "nav-menu item") {
         x.className += " responsive";
         icon.setAttribute("src", "icons/menu_close.png");
-        /* icon.setAttribute("style", "margin:10px;"); */
         another.setAttribute("style", "display:none;")
     } else {
         x.className = "nav-menu item";
-        /* icon.textContent = "مشاهده منو"; */
         icon.setAttribute("src", "icons/menu.png");
         another.setAttribute("style", "display:block;")
     }
@@ -79,7 +77,6 @@ function menueffect(icon) {
 /* for correct show count items of slider to diffrent view */
 window.addEventListener("resize", getwidth);
 var width = 0;
-var countOfSlide = 1;
 function getwidth() {
     width = window.outerWidth;
     let slides = document.getElementsByClassName("flex-item slider");
@@ -88,7 +85,6 @@ function getwidth() {
             slides[i].style.display = "block";
         }
         slideIndex = 0;
-        countOfSlide = 3;
     } else {
         slides[0].style.display = "block";
         for (let i = 1; i < slides.length; i++) {
@@ -96,7 +92,6 @@ function getwidth() {
 
         }
         slideIndex = 0;
-        countOfSlide = 1;
     }
 }
 
@@ -116,7 +111,6 @@ function previous() {
 function showSlides(y) {
     let slides = document.getElementsByClassName("flex-item slider");
     width = window.outerWidth;
-    /* console.log(slides.length); */
     /* Check screen size for type of next and previous behavior */
     if (width < "768") {
         if (slideIndex >= slides.length) {
@@ -146,26 +140,22 @@ function showSlides(y) {
         
         if (y === "next") {
             slides[slideIndex - 1].style.display = "none";
-            console.log(slides[slideIndex]);
             if (slideIndex >= slides.length) {slideIndex = 0}
             var z = slideIndex;
             for (i = 0; i < 3; i++) {
                 slides[z].setAttribute("style", "order:" + (i + 2) + ";");
                 slides[z].style.display = "block";
-                console.log(slides[z]);
                 z += 1;
                 if (z >= slides.length) {z = 0}
                 
             }
         } else {
             slides[slideIndex + 1].style.display = "none";
-            console.log(slides[slideIndex]);
             if (slideIndex < 0) {slideIndex = slides.length - 1}
             var z = slideIndex;
             for (i = 3; i > 0; i--) {
                 slides[z].setAttribute("style", "order:" + (i + 2) + ";");
                 slides[z].style.display = "block";
-                console.log(slides[z]);
                 z -= 1;
                 if (z < 0) {z = slides.length - 1}
                 
